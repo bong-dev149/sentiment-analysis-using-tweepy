cb5a39d38a8a63901012b802eb38e33352de4dbf
Aditya Sadhukhan <64201418+bong-dev149@users.noreply.github.com>
2021-08-27 18:19:42 +0530

    Delete fetch.py

--- sentiment-analysis-using-tweepy-v1.0/fetch.py
+++
@@ -1,8 +0,0 @@
-import tweepy
-import cred
-import textblob
-
-auth = tweepy.AppAuthHandler(cred.API_KEY,cred.API_SECRET)
-api = tweepy.API(auth)
-for tweet in tweepy.Cursor(api.search, q='linux').items(100):
-    print(tweet)
